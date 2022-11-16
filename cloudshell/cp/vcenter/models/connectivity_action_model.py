6ab7d789a7f5ba0534118a7f1b462d5c1a4c6740
Kyrylo Maksymenko <saklar13@gmail.com>
2022-11-16 14:52:03 +0100

    Port Group Name attribute of the VLAN Service is deprecated, use Virtual Network instead

--- cloudshell/cp/vcenter/models/connectivity_action_model.py
+++ cloudshell/cp/vcenter/models/connectivity_action_model.py
@@ -1,3 +1,4 @@
+import warnings
 from typing import Optional
 
 from pydantic import Field
@@ -11,6 +12,15 @@ from cloudshell.shell.flows.connectivity.models.connectivity_model import (
 
 class VcenterVlanServiceModel(VlanServiceModel):
     port_group_name: Optional[str] = Field(None, alias="Port Group Name")
+
+    def __getattribute__(self, item):
+        if "port_group_name" == item:
+            msg = (
+                "'Port Group Name' attribute is deprecated, "
+                "use 'Virtual Network' instead"
+            )
+            warnings.warn(msg, DeprecationWarning, stacklevel=2)
+        return super().__getattribute__(item)
 
 
 class VcenterConnectionParamsModel(ConnectionParamsModel):
